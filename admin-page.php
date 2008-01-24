8a619349f1ade5d7191ec1c0f53f4da94b7f228a
ezra <ezra@afa44554-0db9-64d6-dc80-ec3199b23f77>
2008-01-24 00:47:29 +0000

    setup.php was renamed to config.php; remove obsolete admin-page.php.
    git-svn-id: svn+ssh://dev.getsatisfaction.com/svn/sprinkles/trunk@30 afa44554-0db9-64d6-dc80-ec3199b23f77

--- admin-page.php
+++
@@ -1,5 +0,0 @@
-<?
-global $fields;
-$fields = array('background_color', 'contact_email', 'contact_phone',
-                'contact_address', 'map_url');
-?>
--- handle-submit.php
+++ handle-submit.php
@@ -1,6 +1,6 @@
 <?
 
-require_once('setup.php');
+require_once('config.php');
 require_once('Sprinkles.php');
 
 $subject = request_param('subject');
